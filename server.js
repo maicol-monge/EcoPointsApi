dee3bce850f32d760f72accdf2e38d9d008757e3
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-09-27 20:38:50 -0600

    Hello World

---
+++ server.js
@@ -0,0 +1,51 @@
+require("dotenv").config(); //Cargar variables d entorno
+const express = require("express"); //Crear servidor web
+const cors = require("cors"); //Para permitir solicitudes desde otro dominio
+
+const db = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const pacienteRoutes = require("./routes/pacienteRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const medicoRoutes = require("./routes/medicoRoutes"); // <-- NUEVO
+
+const app = express(); //Instancia del servidor
+ //Evitar errores al consumir en React
+const allowedOrigins = [
+  'https://controlcitas-frontend-production.up.railway.app',
+  'http://localhost:5173', // por ejemplo para desarrollo local
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Permitir solicitudes sin origin (como curl o Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('No permitido por CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+app.use(express.json()); //Recibir los datos en JSON
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error conectando a la base de datos:", err);
+    process.exit(1); // Sale de la aplicación en caso de error
+  }
+  console.log("Conectado a la base de datos MySQL");
+});
+
+// Iniciar servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor backend corriendo en el puerto ${PORT}`);
+});
+
+// Rutas
+app.use("/api/usuarios", userRoutes); //Ruta para el registro de usuarios
+app.use("/api/pacientes", pacienteRoutes); //Ruta para el registro de usuarios
+app.use("/api/admin", adminRoutes); //Ruta para administración
+app.use("/api/medico", medicoRoutes); // <-- NUEVO
